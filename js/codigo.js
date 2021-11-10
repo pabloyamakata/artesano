acdb71c0eed811ca960d902e23aad50f12aa4523
Pablo Yamakata <pablo.yamakata@gmail.com>
2021-11-09 23:22:49 -0300

    Agregado el menú hamburguesa

---
+++ js/codigo.js
@@ -0,0 +1,30 @@
+// Menú Hamburguesa
+
+let btnAbrir = document.getElementById('btn-abrir');
+btnAbrir.addEventListener('click', () => {
+
+    let menu = document.getElementById('menu_header'); 
+    let btnCerrar = document.getElementById('btn-cerrar'); 
+
+    menu.style.display = 'block';
+    btnAbrir.style.display = 'none';
+    btnCerrar.style.display = 'block';
+    btnCerrar.style.float = 'right';
+    btnCerrar.style.marginTop = '15px';
+    btnCerrar.style.marginRight = '20px';
+    btnCerrar.style.cursor = 'pointer';
+    btnCerrar.style.fontSize = '20px';
+    btnCerrar.style.color = 'rgb(255,240,219)';
+})
+
+let btnCerrar = document.getElementById('btn-cerrar');
+btnCerrar.addEventListener('click', () => {
+
+    let menu = document.getElementById('menu_header');
+    let btnAbrir = document.getElementById('btn-abrir'); 
+
+    menu.style.display = 'none';
+    btnAbrir.style.display = 'block';
+    btnCerrar.style.display = 'none';
+})
+
